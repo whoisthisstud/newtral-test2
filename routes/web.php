9268c24194eb5ea8d286262bde771b9bbfe0d6a3
Nailed It! <44807533+whoisthisstud@users.noreply.github.com>
2024-05-23 17:53:00 -0500

    removed unneeded route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewtralUniqueClientsFromLoadsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\NewtralUniqueClientsFromLoadsController')->name('newtral');
-
-
-//Route::get('/{code:short_code}', 'App\Http\Controllers\NewtralController')->name('newtral.redirect');
+Route::get('/', NewtralUniqueClientsFromLoadsController::class )->name('newtral');
